f0c4756e459f0d8765e9927f3f41ee5532a6c8e5
urvesh2212 <urveshmewada3@gmail.com>
2021-03-27 15:50:56 +0530

    header and footer updated

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -7,4 +7,179 @@
             <li class="active">About Us</li>
         </ul>
     </div>
+    <div class="page-content-area">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-12">
+                  <!--=======  page wrapper  =======-->
+                  <div class="page-wrapper">
+                      <div class="page-content-wrapper">
+                          <div class="row">
+
+                              <div class="col-lg-12">
+                                  <div class="about-top-content-wrapper">
+                                      <div class="row row-30">
+                                          <!-- About Image -->
+                                          <div class="col-lg-6">
+                                              <div class="about-image">
+                                                  <img src="assets/img/banners/img2-middle-eposi1.jpg" class="img-fluid" alt="">
+                                              </div>
+                                          </div>
+
+                                          <!-- About Content -->
+                                          <div class="about-content col-lg-6">
+                                              <div class="row">
+                                                  <div class="col-12">
+                                                      <h1>WELCOME TO <span>Eposi.</span></h1>
+                                                      <p class="mb-3">Eposi provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a complete account of the system, and expound the actual teachings of the eat explorer of the truth, the mer of human.</p>
+                                                  </div>
+
+                                                  <div class="col-12">
+                                                      <h4>WIN BEST ONLINE SHOP AT 2019</h4>
+                                                      <p>Eposi provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a complete account of the system, and expound the actual teachings of the eat explorer of the truth, the mer of human.</p>
+                                                  </div>
+
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+
+
+                          </div>
+
+                          <div class="row row-10">
+
+                              <!-- Banner -->
+                              <div class="col-md-4">
+                                  <div class="single-banner">
+                                      <a href="#"><img src="assets/img/banners/banner7.jpg" class="img-fluid" alt="Banner"></a>
+                                  </div>
+                              </div>
+                              <div class="col-md-4 col-12">
+                                  <div class="single-banner">
+                                      <a href="#"><img src="assets/img/banners/banner8.jpg" class="img-fluid" alt="Banner"></a>
+                                  </div>
+                              </div>
+                              <div class="col-md-4 col-12">
+                                  <div class="single-banner">
+                                      <a href="#"><img src="assets/img/banners/banner9.jpg" class="img-fluid" alt="Banner"></a>
+                                  </div>
+
+                              </div>
+
+                          </div>
+
+                          <!-- Mission, Vission & Goal -->
+                          <div class="about-mission-vission-goal row row-20">
+
+                              <div class="col-lg-4 col-md-4 col-12">
+                                  <h3>OUR VISSION</h3>
+                                  <p>Eposi provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a ete account of the system, and expound the actual teangs the eat explorer of the truth, the mer of human tas assumenda est, omnis dolor repellend</p>
+                              </div>
+
+                              <div class="col-lg-4 col-md-4 col-12">
+                                  <h3>OUR MISSION</h3>
+                                  <p>Eposi provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a ete account of the system, and expound the actual teangs the eat explorer of the truth, the mer of human tas assumenda est, omnis dolor repellend</p>
+                              </div>
+
+                              <div class="col-lg-4 col-md-4 col-12">
+                                  <h3>OUR GOAL</h3>
+                                  <p>Eposi provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a ete account of the system, and expound the actual teangs the eat explorer of the truth, the mer of human tas assumenda est, omnis dolor repellend</p>
+                              </div>
+
+                          </div>
+
+                          <div class="row">
+
+                              <!-- About Section Title -->
+                              <div class="about-section-title col-12">
+                                  <h3>YOU CAN CHOOSE US BECAUSE <br>WE ALWAYS PROVIDE IMPORTANCE...</h3>
+                                      <p>Eposi provide how all this mistaken idea of denouncing pleasure and sing pain was born will give you a complete account of the system, and expound the actual</p>
+                              </div>
+
+                              <!-- About Feature -->
+                              <div class="about-feature col-md-7 col-12 mb-sm-30">
+                                  <div class="row">
+
+                                      <div class="col-md-6 col-12">
+                                          <h4>FAST DELIVERY</h4>
+                                          <p>Eposi provide how all this mistaken dea of denouncing pleasure and sing </p>
+                                      </div>
+
+                                      <div class="col-md-6 col-12">
+                                          <h4>QUALITY PRODUCT</h4>
+                                          <p>Eposi provide how all this mistaken dea of denouncing pleasure and sing </p>
+                                      </div>
+
+                                      <div class="col-md-6 col-12">
+                                          <h4>SECURE PAYMENT</h4>
+                                          <p>Eposi provide how all this mistaken dea of denouncing pleasure and sing </p>
+                                      </div>
+
+                                      <div class="col-md-6 col-12">
+                                          <h4>MONEY BACK GUARNTEE</h4>
+                                          <p>Eposi provide how all this mistaken dea of denouncing pleasure and sing </p>
+                                      </div>
+
+                                      <div class="col-md-6 col-12">
+                                          <h4>EASY ORDER TRACKING</h4>
+                                          <p>Eposi provide how all this mistaken dea of denouncing pleasure and sing </p>
+                                      </div>
+
+                                      <div class="col-md-6 col-12">
+                                          <h4>FREE RETURN</h4>
+                                          <p>Eposi provide how all this mistaken dea of denouncing pleasure and sing </p>
+                                      </div>
+
+                                      <div class="col-md-6 col-12">
+                                          <h4>24/7 SUPPORT</h4>
+                                          <p class="mb-md-0">Eposi provide how all this mistaken dea of denouncing pleasure and sing </p>
+                                      </div>
+
+                                  </div>
+                              </div>
+
+                              <!-- About Feature Banner -->
+                              <div class="about-feature-banner col-md-5 col-12">
+                                  <div class="single-banner mb-0"><a href="#"><img src="assets/img/banners/slider-banner2.jpg" class="img-fluid" alt="Banner"></a></div>
+                              </div>
+
+                          </div>
+                      </div>
+                  </div>
+                  <!--=======  End of page wrapper  =======-->
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="newsletter-area section-space--inner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="newsletter-wrapper">
+                    <p class="small-text">Special Ofers For Subscribers</p>
+                    <h3 class="title">Ten Percent Member Discount</h3>
+                    <p class="short-desc">Subscribe to our newsletters now and stay up to date with new collections, the latest lookbooks and exclusive offers.</p>
+
+                    <div class="newsletter-form">
+                        <form id="mc-form" class="mc-form">
+                            <input type="email" placeholder="Enter Your Email Address Here..." required>
+                            <button type="submit" value="submit">SUBSCRIBE</button>
+                        </form>
+
+                    </div>
+                    <!-- mailchimp-alerts Start -->
+                    <div class="mailchimp-alerts">
+                        <div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
+                        <div class="mailchimp-success"></div><!-- mailchimp-success end -->
+                        <div class="mailchimp-error"></div><!-- mailchimp-error end -->
+                    </div>
+                    <!-- mailchimp-alerts end -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
     @endsection
---
+++ resources/views/front/mobilelogin.blade.php
@@ -0,0 +1,20 @@
+@extends('front.root')
+  @section('content')
+  <div class="page-content-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <!--=======  page wrapper  =======-->
+                <div class="page-wrapper">
+                    <div class="page-content-wrapper">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  @endsection
